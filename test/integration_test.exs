b0d9ec2c80a0ace2ea6265db4cbfc19f4021681c
Adam Rutkowski <hq@mtod.org>
2022-08-21 13:38:13 +0200

    Fixup supervisor tests

--- test/integration_test.exs
+++ test/integration_test.exs
@@ -5,26 +5,22 @@ defmodule Airbag.IntegrationTest do
 
   test "supervisor spec" do
     assert spec =
-             Supervisor.child_spec(
-               {Airbag.Supervisor,
-                %{
-                  buffer_name: TestBuffer,
-                  partition_count: 1,
-                  consumers_per_partition: 1,
-                  dequeue_limit: 10_000,
-                  total_memory_threshold: 1024 * 1024 * 1024,
-                  processor: fn _messages -> :timer.sleep(500) end
-                }},
-               id: TestBufferSupervisorId
+             Airbag.child_spec(
+               buffer_name: TestBuffer,
+               partition_count: 1,
+               consumers_per_partition: 1,
+               dequeue_limit: 10_000,
+               total_memory_threshold: 1024 * 1024 * 1024,
+               processor: fn _messages -> :timer.sleep(500) end
              )
 
-    assert spec.id == TestBufferSupervisorId
+    assert spec.id == TestBuffer
     assert {Airbag.Supervisor, :start_link, [init_arg]} = spec.start
-    assert init_arg.buffer_name == TestBuffer
-    assert init_arg.consumers_per_partition == 1
-    assert init_arg.dequeue_limit == 10_000
-    assert init_arg.partition_count == 1
-    assert is_function(init_arg.processor, 1)
+    assert Keyword.get(init_arg, :buffer_name) == TestBuffer
+    assert Keyword.get(init_arg, :consumers_per_partition) == 1
+    assert Keyword.get(init_arg, :dequeue_limit) == 10_000
+    assert Keyword.get(init_arg, :partition_count) == 1
+    assert is_function(Keyword.get(init_arg, :processor), 1)
   end
 
   test "supervisor starts buffers and consumers" do
@@ -37,7 +33,7 @@ defmodule Airbag.IntegrationTest do
       processor: fn got -> send(test_pid, {:processed, got}) end
     ]
 
-    {:ok, supervisor} = Airbag.Supervisor.start_link(init_arg)
+    {:ok, _supervisor} = Airbag.Supervisor.start_link(init_arg)
     Buffer.enqueue(TestBuffer, :first_message)
     assert_receive {:processed, [:first_message]}
   end
