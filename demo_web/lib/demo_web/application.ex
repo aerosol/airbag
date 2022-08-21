86983835617390d4f5597975db7652806e505601
Adam Rutkowski <hq@mtod.org>
2022-08-21 08:43:28 +0200

    Simplify child specification

--- demo_web/lib/demo_web/application.ex
+++ demo_web/lib/demo_web/application.ex
@@ -8,31 +8,26 @@ defmodule DemoWeb.Application do
     :ok = Airbag.LogTelemetry.init()
 
     children = [
-      Supervisor.child_spec(
-        {Airbag.Supervisor,
-         %{
-           buffer_name: DemoBufferSingleton,
-           partition_count: 1,
-           consumers_per_partition: 1,
-           dequeue_limit: 10_000,
-           total_memory_threshold: 1024 * 1024 * 1024,
-           processor: fn _messages -> :timer.sleep(500) end,
-           interval: 500
-         }},
-        id: DemoBufferSingletonSupervisor
-      ),
-      Supervisor.child_spec(
-        {Airbag.Supervisor,
-         %{
-           buffer_name: DemoSchedulersSingleton,
-           partition_count: System.schedulers_online(),
-           consumers_per_partition: System.schedulers_online(),
-           total_memory_threshold: 1_000_000,
-           dequeue_limit: 1,
-           processor: fn _message -> IO.write(".") end
-         }},
-        id: DemoBufferSchedulersSupervisor
-      ),
+      {Airbag,
+       [
+         buffer_name: DemoBufferSingleton,
+         partition_count: 1,
+         consumers_per_partition: 1,
+         dequeue_limit: 10_000,
+         total_memory_threshold: 1024 * 1024 * 1024,
+         processor: fn _messages -> :timer.sleep(500) end,
+         interval: 500
+       ]},
+      {Airbag,
+       [
+         buffer_name: DemoBufferPartitioned,
+         partition_count: System.schedulers_online(),
+         consumers_per_partition: System.schedulers_online(),
+         dequeue_limit: 10_000,
+         total_memory_threshold: 1024 * 1024 * 1024,
+         processor: fn _messages -> :timer.sleep(500) end,
+         interval: 500
+       ]},
       {Plug.Cowboy, scheme: :http, plug: DemoWeb.Router, options: [port: 8099]}
     ]
 
--- lib/airbag.ex
+++ lib/airbag.ex
@@ -1,2 +1,8 @@
 defmodule Airbag do
+  def child_spec(opts) do
+    %{
+      id: Keyword.fetch!(opts, :buffer_name),
+      start: {Airbag.Supervisor, :start_link, [opts]}
+    }
+  end
 end
